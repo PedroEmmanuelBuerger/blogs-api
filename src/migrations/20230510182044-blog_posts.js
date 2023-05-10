02f7397c505911d617d7ccebdb90dc9df3bd30c6
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-05-10 16:32:37 -0300

    requesito 1

---
+++ src/migrations/20230510182044-blog_posts.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  /**
+   * 
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize')} Sequelize 
+   */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
